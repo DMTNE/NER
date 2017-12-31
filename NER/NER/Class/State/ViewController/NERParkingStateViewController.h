0e676c688fca84a3c5f4d206abd38f192ccc9f01
方静雯 <fangjingwen@fangjingwendeMacBook-Pro.local>
2017-12-31 12:51:44 +0800

    删除状态页面

--- NER/NER/Class/State/ViewController/NERParkingStateViewController.h
+++ NER/NER/Class/State/ViewController/NERParkingStateViewController.h
@@ -2,12 +2,12 @@
 //  NERParkingStateViewController.h
 //  NER
 //
-//  Created by Fangjw on 2017/12/27.
+//  Created by 方静雯 on 2017/12/31.
 //  Copyright © 2017年 New Energy. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface NERParkingStateViewController : UIViewController
+@interface NERParkingStateViewController : UITableViewController
 
 @end
--- NER/NER/Class/State/ViewController/NERParkingStateViewController.m
+++ NER/NER/Class/State/ViewController/NERParkingStateViewController.m
@@ -2,16 +2,13 @@
 //  NERParkingStateViewController.m
 //  NER
 //
-//  Created by Fangjw on 2017/12/27.
+//  Created by 方静雯 on 2017/12/31.
 //  Copyright © 2017年 New Energy. All rights reserved.
 //
 
 #import "NERParkingStateViewController.h"
-#import "NERParkingStateTableView.h"
 
 @interface NERParkingStateViewController ()
-
-@property (nonatomic, strong)NERParkingStateTableView *tableView;
 
 @end
 
@@ -19,7 +16,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -27,19 +29,70 @@
     // Dispose of any resources that can be recreated.
 }
 
--(void)viewWillAppear:(BOOL)animated{
-    [super viewWillAppear:animated];
-    self.navigationController.navigationBar.topItem.title=@"停车状态";
-    [self createView];
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+#warning Incomplete implementation, return the number of sections
+    return 0;
 }
 
--(void)createView{
-    _tableView = [[NERParkingStateTableView alloc] initWithFrame:CGRectNull style:UITableViewStylePlain];
-    _tableView.backgroundColor = [UIColor whiteColor];
-    [self.view addSubview:_tableView];
-    [_tableView mas_makeConstraints:^(MASConstraintMaker *make){
-        make.edges.mas_equalTo(UIEdgeInsetsMake(0, 0, 0, 0));
-    }];
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#warning Incomplete implementation, return the number of rows
+    return 0;
 }
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
 
 @end
